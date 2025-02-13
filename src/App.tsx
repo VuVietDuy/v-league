4c3a55630cdb8c04c1726705e7cd1023242bcde5
unknown <lamtx.3107@gmail.com>
2025-02-13 17:40:21 +0700

    fix redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Home from "@/pages/Home/Home";
 import MatchDetails from "@/pages/Match/MatchDetails/MatchDetails";
@@ -18,7 +19,7 @@ import Results from "@/pages/Match/Results";
 import Category from "@/pages/Category/Category";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { store } from "@/store/store";
+import { persistor, store } from "@/store/store";
 import Search from "./pages/Search/Search";
 
 const queryClient = new QueryClient();
@@ -26,31 +27,33 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <div className="font-montserrat">
-            <Navbar />
-            <Routes>
-              <Route path="/search" element={<Search />} />
-              <Route path="" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/:tournamentId/fixtures/" element={<Fixtures />} />
-              <Route path="/:tournamentId/results/" element={<Results />} />
-              <Route path="/:tournamentId/tables/" element={<Tables />} />
-              <Route path="/:tournamentId/clubs/" element={<ClubList />} />
-              <Route path="/clubs/:clubId/*" element={<ClubDetail />} />
-              <Route path="/players" element={<PlayerList />} />
-              <Route path="/players/:playerId" element={<PlayerDetail />} />
-              <Route path="/news" element={<NewsList />} />
-              <Route path="/news/:newsId" element={<NewsDetail />} />
-              <Route path="/match/:matchId/*" element={<MatchDetails />} />
-              <Route path="/:tournamentId/category" element={<Category />} />
-            </Routes>
-            <Footer />
-          </div>
-        </BrowserRouter>
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <div className="font-montserrat">
+              <Navbar />
+              <Routes>
+                <Route path="/search" element={<Search />} />
+                <Route path="" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/:tournamentId/fixtures/" element={<Fixtures />} />
+                <Route path="/:tournamentId/results/" element={<Results />} />
+                <Route path="/:tournamentId/tables/" element={<Tables />} />
+                <Route path="/:tournamentId/clubs/" element={<ClubList />} />
+                <Route path="/clubs/:clubId/*" element={<ClubDetail />} />
+                <Route path="/players" element={<PlayerList />} />
+                <Route path="/players/:playerId" element={<PlayerDetail />} />
+                <Route path="/news" element={<NewsList />} />
+                <Route path="/news/:newsId" element={<NewsDetail />} />
+                <Route path="/match/:matchId/*" element={<MatchDetails />} />
+                <Route path="/:tournamentId/category" element={<Category />} />
+              </Routes>
+              <Footer />
+            </div>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   );
 }
